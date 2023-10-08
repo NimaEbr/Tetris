49f8b4a4295e35204acea2eb58bec41e765ebdd4
NimaEbr <nima.19992017@gmail.com>
2023-10-08 14:56:50 +0200

    Tetris_Game

---
+++ Tetris2/Headers/GetTetromino.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <vector>
+
+std::vector<Position> get_tetromino(unsigned char i_shape, unsigned char i_x, unsigned char i_y);
---
+++ Tetris2/Headers/GetWallKickData.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <vector>
+
+std::vector<Position> get_wall_kick_data(bool i_is_i_shape, unsigned char i_current_rotation, unsigned char i_next_rotation);
